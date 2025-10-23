4aaefb0b7d7dfe4b353e6628c79d9bd9029967a0
ForagK <148355365+ForagK@users.noreply.github.com>
2025-10-23 22:59:46 +0300

    LR6 fix

--- app/src/main/java/com/lyannyi/lr6/viewmodel/PlayerViewModel.kt
+++ app/src/main/java/com/lyannyi/lr6/viewmodel/PlayerViewModel.kt
@@ -61,4 +61,11 @@ class PlayerViewModel() : ViewModel() {
         mediaPlayer?.release()
         mediaPlayer = null
     }
+
+    fun formatTime(ms: Int): String {
+        val totalSeconds = ms / 1000
+        val minutes = totalSeconds / 60
+        val seconds = totalSeconds % 60
+        return String.format("%02d:%02d", minutes, seconds)
+    }
 }
